cd3d33e691f1e991030581bda5cc6dfec08737b6
wangyi2070@outlook.com <wangyi2070@163.com>
2018-03-02 23:13:20 -0500

    update

--- src/main/java/com/tom/cloud/confsvr/ConfigServerApplication.java
+++ src/main/java/com/tom/cloud/confsvr/ConfigServerApplication.java
@@ -3,15 +3,10 @@ package com.tom.cloud.confsvr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-<<<<<<< HEAD
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableEurekaClient
-=======
-
 @SpringBootApplication
->>>>>>> branch 'master' of https://github.com/brightsailor/cofig-repository.git
 @EnableConfigServer
 public class ConfigServerApplication {
 
